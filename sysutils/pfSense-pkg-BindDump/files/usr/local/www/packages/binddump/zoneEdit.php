3bc03bea4c36c7b484cb8e5dfbd819ec095694b7
Andreas W. Pross <andreas.pross@styletronix.net>
2023-05-02 02:20:41 +0200

    Optimize Zone Edit layoout

--- sysutils/pfSense-pkg-BindDump/files/usr/local/www/packages/binddump/zoneEdit.php
+++ sysutils/pfSense-pkg-BindDump/files/usr/local/www/packages/binddump/zoneEdit.php
@@ -14,7 +14,7 @@ $rndc = "/usr/local/sbin/rndc -q -c {$rndc_conf_path}";
 if ($_POST) {
     $input_errors = array();
     $post = $_POST;
-    
+
     if (!empty($_POST["thawall"])) {
         exec("{$rndc} thaw" . ' 2>&1', $output, $resultCode);
         if ($resultCode !== 0) {
@@ -79,7 +79,7 @@ if ($_POST) {
         }
     }
 
-    
+
 
     if (!empty($_POST["freeze"])) {
         exec("{$rndc} freeze " . escapeshellarg($zonename_reverse) . " IN " . escapeshellarg($zoneview) . ' 2>&1', $output, $resultCode);
@@ -106,8 +106,9 @@ display_top_tabs($tab_array);
 
 $zonelist = [];
 foreach (binddump_get_zonelist() as $zone) {
-    $zonelist[$zone['view'] . '__' . $zone['name'] . '__' . binddump_reverse_zonename($zone) . '__' . $zone['type']] = $zone['view'] . ': ' . binddump_reverse_zonename($zone);
+    $zonelist[$zone['view'] . '__' . $zone['name'] . '__' . binddump_reverse_zonename($zone) . '__' . $zone['type']] = binddump_reverse_zonename($zone) . '  (' . $zone['view'] . ')' ;
 }
+ksort($zonelist);
 
 if ($input_errors) {
     print_input_errors($input_errors);
@@ -117,6 +118,130 @@ if ($savemsg) {
     print_info_box($savemsg, 'success');
 }
 
+?>
+
+<form method="post" action="zoneEdit.php" enctype="multipart/form-data">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h2 class="panel-title"><?=gettext('Zone')?></h2>
+        </div>
+        <div class="panel-body">
+            <div class="form-group">
+                <label for="zoneselect" class="col-sm-2 control-label" onchange="this.form.submit();">
+                    <span><?=gettext('Zone')?></span>
+                </label>
+                <div class="col-sm-10">
+                    <select class="form-control" name="zoneselect" id="zoneselect" onchange="this.form.submit();">
+                        <option value=""><?=gettext('Select Zone...')?></option>
+                        <? foreach ($zonelist as $key => $value) { ?>
+                            <option <? if ($key == $post['current_zone']){print('selected');} ?> value="<?=$key?>"><?=$value?></option>
+                        <? } ?>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="load" class="col-sm-2 control-label">
+                    <span><?=gettext('Actions')?></span>
+                </label>
+                <div class="col-sm-2">
+                    <input class="btn btn-primary" type="submit" value="<?=gettext('Reload Zone')?>" name="load" id="load">
+                </div>
+                <div class="col-sm-2">
+                    <input class="btn btn-primary" type="submit" value="<?=gettext('Start Edit')?>" name="freeze" id="freeze">
+                    <span class="help-block"><?=gettext('While in Edit-Mode, DDNS Updates are disabled.')?></span>
+                </div>
+                <div class="col-sm-2">
+                    <input class="btn btn-primary" type="submit" value="<?=gettext('End Edit')?>" name="thaw" id="thaw">
+                </div>
+                <div class="col-sm-2">
+                    <input class="btn btn-primary" type="submit" value="<?=gettext('End Edit')?> -- <?=gettext('All Zones')?>" name="thawall" id="thawall">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h2 class="panel-title"><?=gettext('Edit Zone')?></h2>
+        </div>
+        <div class="panel-body">
+            <textarea rows="25" class="form-control" name="zone_data" id="zone_data" wrap="off" <? if ($post['zone_editable'] !== 'true') {print('readonly="readonly"'); } ?> >
+            <?= $post['zone_data'] ?>
+        </textarea>
+        </div>
+    </div>
+
+    <div id="dlg_updatestatus" class="modal fade" role="dialog" aria-labelledby="dlg_updatestatus" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                    <h3 class="modal-title">Aktion</h3>
+                </div>
+                <!--				<form class="form-horizontal" action="" method="post"><input type='hidden' name='__csrf_magic' value="sid:559d4e4b09aef7ecd19d95821c0d10a3d4114043,1682931991" /> -->
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="dlg_updatestatus_text" class="col-sm-2 control-label">
+
+                        </label>
+                        <div class="col-sm-10">
+                            <textarea rows="10" class="row-fluid col-sm-10" name="dlg_updatestatus_text"
+                                id="dlg_updatestatus_text" wrap="off">...Loading...</textarea>
+
+
+                        </div>
+
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <input class="btn btn-primary" type="submit" value="Schliessen" name="save" id="save"
+                        data-dismiss="modal">
+                </div>
+                <!--				</form> -->
+            </div>
+        </div>
+    </div>
+    <div id="dlg_wait" class="modal fade" role="dialog" aria-labelledby="dlg_wait" aria-hidden="true">
+        <div class="modal-dialog modal-sm">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                    <h3 class="modal-title">Please wait</h3>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">
+                            <span>dlg_wait_text</span>
+                        </label>
+                        <div class="col-sm-10">
+                            Please wait for the process to complete.<br><br>This dialog will auto-close when the update
+                            is finished.<br><br><i
+                                class="content fa fa-spinner fa-pulse fa-lg text-center text-info"></i>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <input class="btn btn-primary" type="submit" value="Schliessen" name="save" id="save"
+                        data-dismiss="modal">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <input class="form-control" name="current_zone" id="current_zone" type="hidden" value="<?= $post['current_zone'] ?>" />
+    <input class="form-control" name="zone_editable" id="zone_editable" type="hidden" value="<?= $post['zone_editable'] ?>" />
+
+    <div class="col-sm-10 col-sm-offset-2">
+        <button class="btn btn-primary" type="submit" value="Speichern" name="save" id="save"><i class="fa fa-save icon-embed-btn"> </i>Speichern</button>
+    </div>
+</form>
+
+<?
 $form = new Form();
 $form->setMultipartEncoding();
 
@@ -127,73 +252,74 @@ $zoneselect = new Form_Select('zoneselect', 'Zone', $post['zoneselect'], $zoneli
 $zoneselect->setOnchange("this.form.submit();");
 $section->addInput($zoneselect);
 
-$group = new Form_Group('Action');
+// $group = new Form_Group('Action');
 
-$btnreload = new Form_Button('load', 'Reload Zone');
-$group->add($btnreload);
+// $btnreload = new Form_Button('load', 'Reload Zone');
+// $group->add($btnreload);
 
-$btnfreeze = new Form_Button('freeze', 'Start Edit');
-$btnfreeze->setHelp('While in Edit-Mode, DDNS Updates are disabled.');
-$group->add($btnfreeze);
+// $btnfreeze = new Form_Button('freeze', 'Start Edit');
+// $btnfreeze->setHelp('While in Edit-Mode, DDNS Updates are disabled.');
+// $group->add($btnfreeze);
 
-$btnthaw = new Form_Button('thaw', 'End Edit');
-$group->add($btnthaw);
+// $btnthaw = new Form_Button('thaw', 'End Edit');
+// $group->add($btnthaw);
 
-$btnthawall = new Form_Button('thawall', 'End Edit - ALL ZONES');
-$group->add($btnthawall);
+// $btnthawall = new Form_Button('thawall', 'End Edit - ALL ZONES');
+// $group->add($btnthawall);
 
-$section->add($group);
-$form->add($section);
-/* #endregion */
+// $section->add($group);
+// $form->add($section);
+// /* #endregion */
 
-/* #region section Edit Zone */
-$section = new Form_Section('Edit Zone');
-$zonetext = new Form_Textarea(
-    'zone_data',
-    'Zone File',
-    $post['zone_data']
-);
-$zonetext->setWidth(8);
-$zonetext->setAttribute("rows", "25");
-$zonetext->setAttribute("wrap", "off");
-if ($post['zone_editable'] !== "true") {
-    $zonetext->setReadonly();
-}
-$section->addInput($zonetext);
+// /* #region section Edit Zone */
+// $section = new Form_Section('Edit Zone');
+// $zonetext = new Form_Textarea(
+//     'zone_data',
+//     'Zone File',
+//     $post['zone_data']
+// );
+// $zonetext->setWidth(8);
+// $zonetext->setAttribute("rows", "25");
+// $zonetext->setAttribute("wrap", "off");
+// if ($post['zone_editable'] !== "true") {
+//     $zonetext->setReadonly();
+// }
+// $section->addInput($zonetext);
 
-$form->add($section);
-/* #endregion */
+// $form->add($section);
+// /* #endregion */
 
-$form->addGlobal(new Form_Input('current_zone', null, 'hidden', $post['current_zone']));
-$form->addGlobal(new Form_Input('zone_editable', null, 'hidden', $post['zone_editable']));
+// $form->addGlobal(new Form_Input('current_zone', null, 'hidden', $post['current_zone']));
+// $form->addGlobal(new Form_Input('zone_editable', null, 'hidden', $post['zone_editable']));
 
-/* #region Create a Modal Dialog */
-$modal = new Modal('Aktion', 'dlg_updatestatus', 'large', 'Close');
-$modal->addInput(
-    new Form_Textarea(
-        'dlg_updatestatus_text',
-        '',
-        '...Loading...'
-    )
-)->removeClass('form-control')->addClass('row-fluid col-sm-10')->setAttribute('rows', '10')->setAttribute('wrap', 'off');
-$form->add($modal);
+// /* #region Create a Modal Dialog */
+// $modal = new Modal('Aktion', 'dlg_updatestatus', 'large', 'Close');
+// $modal->addInput(
+//     new Form_Textarea(
+//         'dlg_updatestatus_text',
+//         '',
+//         '...Loading...'
+//     )
+// )->removeClass('form-control')->addClass('row-fluid col-sm-10')->setAttribute('rows', '10')->setAttribute('wrap', 'off');
+// $form->add($modal);
 
-$modal = new Modal(gettext('Please wait'), 'dlg_wait', false, 'Close');
-$modal->addInput(
-    new Form_StaticText(
-        'dlg_wait_text',
-        'Please wait for the process to complete.<br/><br/>This dialog will auto-close when the update is finished.<br/><br/>' .
-        '<i class="content fa fa-spinner fa-pulse fa-lg text-center text-info"></i>'
-    )
-);
-$form->add($modal);
-/* #endregion */
+// $modal = new Modal(gettext('Please wait'), 'dlg_wait', false, 'Close');
+// $modal->addInput(
+//     new Form_StaticText(
+//         'dlg_wait_text',
+//         'Please wait for the process to complete.<br/><br/>This dialog will auto-close when the update is finished.<br/><br/>' .
+//         '<i class="content fa fa-spinner fa-pulse fa-lg text-center text-info"></i>'
+//     )
+// );
+// $form->add($modal);
+// /* #endregion */
 
 print $form;
 
 ?>
 <script type="text/javascript">
     events.push(function () {
+        // thaw all zones before leaving page
         $(window).on('beforeunload', function () {
             $.ajax({
                 url: 'zoneEdit.php',
